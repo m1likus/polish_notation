4c9ab12a454b48dd43a1ddfb171317ebb53f10a3
m1likus <113242173+m1likus@users.noreply.github.com>
2023-11-29 13:57:58 +0300

    Update polish_notation.h

--- include/polish_notation.h
+++ include/polish_notation.h
@@ -3,6 +3,26 @@
 #include <vector>
 #include <map>
 #include <set>
+
+/*
+currentStatus 				lexemeType 			nextStatus
+waitSt						number				numberSt
+waitSt						bin_operator			errorSt
+waitSt						function			funcSt
+waitSt						end				end
+numberSt					number				errorSt
+numberSt 					bin_operator			binOperatorSt
+numberSt					function			errorSt
+numberSt					end				end
+binOperatorSt					number				numberSt
+binOperatorSt					bin_operator			errorSt
+binOperatorSt					function			funcSt
+binOperatorSt					end				errorSt
+funcSt						number				numberSt
+funcSt						bin_operator			errorSt
+funcSt						function			funcSt
+funcSt						end				errorSt
+*/
 using namespace std;
 bool isDigit(char c) {
 	return (c >= '0' && c <= '9');
@@ -10,25 +30,29 @@ bool isDigit(char c) {
 
 enum TypeLexeme {
 	number,
-	bin_operator
+	bin_operator,
+	function
 };
 
 struct Lexema {
 	int value = 0;
 	TypeLexeme type = TypeLexeme::number;
-	string s;
+	string name;
 };
 
 enum Status {
 	waitSt,
 	numberSt,
-	binOperatorSt
+	binOperatorSt,
+	errorSt,
+	funcSt
 };
 
 vector<Lexema> lexemas;
 
 Lexema getLexema(string input, int pos, int& nextPos) {
 	Lexema lexeme;
+	//if the symbols is number we start searching
 	if (isDigit) {
 		int value = input[pos] - '0';
 		pos++;
@@ -42,6 +66,13 @@ Lexema getLexema(string input, int pos, int& nextPos) {
 		lexeme.value = value;
 		lexeme.type = number;
 	}
+	// if the symbol is a binary operator
+	if (input[pos]=="+" ||  input[pos]=="-" || input[pos]=="*" || input[pos]=="/"){
+		nextPos++;
+		lexeme.type = bin_opeartor;
+		lexeme.name = input[pos];
+	}
+	//if there's function
 	
 	return lexeme;
 }
@@ -52,15 +83,27 @@ vector<Lexema> parse(string input) {
 	vector<Lexema> v;
 	while (next_pos != input.size()) {
 		Lexema l = getLexema(input, pos, next_pos);
-		if (status == waitSt) {
-
-		}
-		else if (status == numberSt) {
-
-		}
-		else if (status == binOperatorSt) {
-
-		}
+	switch(status):
+		case waitSt:
+			if (l.type == number) status = numberSt;
+			else if (l.type==bin_operator) status = errorSt;
+			else if(l.type == function) status = funcSt;
+			else if(next_pos==input.size()) break;
+		case numberSt:
+			if (l.type == number) status = errorSt;
+			else if (l.type==bin_operator) status = binOperatorSt;
+			else if(l.type == function) status = errorSt;
+			else if(next_pos==input.size()) break;
+		case binOperatorSt:
+			if (l.type == number) status = numberSt;
+			else if (l.type==bin_operator) status = errorSt;
+			else if(l.type == function) status = funcSt;
+			else if(next_pos==input.size()) status=errorSt;
+		case funcSt:
+			if (l.type == number) status = numberSt;
+			else if (l.type==bin_operator) status = errorSt;
+			else if(l.type == function) status = funcSt;
+			else if(next_pos==input.size()) status==errorSt;
 	}
 	return v;
 }
@@ -119,47 +162,47 @@ bool bracketCheck(string str) {
 	//	for (int i = 0; i < infix.size(); i++) {
 	//		char l = infix[i];
 	//		if (operations.count(l)) {
-	//			//äâà âàðèàíòà ðàçâèòèÿ ñîáûòèé: ñòðîêà ïóñòàÿ è ñòðîêà íå ïóñòàÿ
-	//			//åñëè ñòðîêà íå ïóñòàÿ,òî íàäî ñíà÷àëà îïðåäåëèòü, ÷òî â íåé áûëî.
-	//			//åñëè ýòî íå ÷èñëî è íå ôóíêöèÿ èç ðàçðåøåííûõ
-	//			//òî âîçíèêàþò âîïðîñû êòî ýòî òàêîé, à çíà÷èò âûáðàñûâàåì îøèáî÷êó.
+	//			//Ã¤Ã¢Ã  Ã¢Ã Ã°Ã¨Ã Ã­Ã²Ã  Ã°Ã Ã§Ã¢Ã¨Ã²Ã¨Ã¿ Ã±Ã®Ã¡Ã»Ã²Ã¨Ã©: Ã±Ã²Ã°Ã®ÃªÃ  Ã¯Ã³Ã±Ã²Ã Ã¿ Ã¨ Ã±Ã²Ã°Ã®ÃªÃ  Ã­Ã¥ Ã¯Ã³Ã±Ã²Ã Ã¿
+	//			//Ã¥Ã±Ã«Ã¨ Ã±Ã²Ã°Ã®ÃªÃ  Ã­Ã¥ Ã¯Ã³Ã±Ã²Ã Ã¿,Ã²Ã® Ã­Ã Ã¤Ã® Ã±Ã­Ã Ã·Ã Ã«Ã  Ã®Ã¯Ã°Ã¥Ã¤Ã¥Ã«Ã¨Ã²Ã¼, Ã·Ã²Ã® Ã¢ Ã­Ã¥Ã© Ã¡Ã»Ã«Ã®.
+	//			//Ã¥Ã±Ã«Ã¨ Ã½Ã²Ã® Ã­Ã¥ Ã·Ã¨Ã±Ã«Ã® Ã¨ Ã­Ã¥ Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã¨Ã§ Ã°Ã Ã§Ã°Ã¥Ã¸Ã¥Ã­Ã­Ã»Ãµ
+	//			//Ã²Ã® Ã¢Ã®Ã§Ã­Ã¨ÃªÃ Ã¾Ã² Ã¢Ã®Ã¯Ã°Ã®Ã±Ã» ÃªÃ²Ã® Ã½Ã²Ã® Ã²Ã ÃªÃ®Ã©, Ã  Ã§Ã­Ã Ã·Ã¨Ã² Ã¢Ã»Ã¡Ã°Ã Ã±Ã»Ã¢Ã Ã¥Ã¬ Ã®Ã¸Ã¨Ã¡Ã®Ã·ÃªÃ³.
 	//			if (!lexema.empty()) {
 	//				if (lexema != sin && lexema != cos && lexema != tg && lexema != ctg
 	//					&& numbers.count(lexema.back()) == 0)
 	//					throw("wrong expression, bye-bye");
-	//				//òåïåðü, çíàÿ, ÷òî ìû íè÷åãî ëèøíåãî â ëåêñåìû íå âáðîñèì
-	//				//êèäàåì òóäà ñíà÷àëà òî, ÷òî áûëî äî îïåðàòîðà â ëåêñåìå
+	//				//Ã²Ã¥Ã¯Ã¥Ã°Ã¼, Ã§Ã­Ã Ã¿, Ã·Ã²Ã® Ã¬Ã» Ã­Ã¨Ã·Ã¥Ã£Ã® Ã«Ã¨Ã¸Ã­Ã¥Ã£Ã® Ã¢ Ã«Ã¥ÃªÃ±Ã¥Ã¬Ã» Ã­Ã¥ Ã¢Ã¡Ã°Ã®Ã±Ã¨Ã¬
+	//				//ÃªÃ¨Ã¤Ã Ã¥Ã¬ Ã²Ã³Ã¤Ã  Ã±Ã­Ã Ã·Ã Ã«Ã  Ã²Ã®, Ã·Ã²Ã® Ã¡Ã»Ã«Ã® Ã¤Ã® Ã®Ã¯Ã¥Ã°Ã Ã²Ã®Ã°Ã  Ã¢ Ã«Ã¥ÃªÃ±Ã¥Ã¬Ã¥
 	//				lexems.push_back(lexema);
 	//				lexema = "";
 	//			}
-	//			//à òåïåðü ìîæåì è ñ íûíåøíåé ëåêñåìîé ðàçîáðàòüñÿ, óðà
+	//			//Ã  Ã²Ã¥Ã¯Ã¥Ã°Ã¼ Ã¬Ã®Ã¦Ã¥Ã¬ Ã¨ Ã± Ã­Ã»Ã­Ã¥Ã¸Ã­Ã¥Ã© Ã«Ã¥ÃªÃ±Ã¥Ã¬Ã®Ã© Ã°Ã Ã§Ã®Ã¡Ã°Ã Ã²Ã¼Ã±Ã¿, Ã³Ã°Ã 
 	//			lexema += l;
 	//			lexems.push_back(lexema);
 	//			lexema = "";
 	//		}
 	//		if (numbers.count(l)) {
-	//			//àíàëîãè÷íî, ñòðîêà ì.á. ïóñòîé è íå ïóñòîé.
-	//			//åñëè íå ïóñòàÿ, òî òàì ëèáî ÷èñëî, ëèáî ôóíêöèÿ
-	//			//ëèáî ÷òî-òî, ÷òî ìû ìîæåì âûáðîñèòü â îøèáêó)
-	//			//îïåðàòîðà òàì áûòü íå ìîæåò, â ïðîøëîì if î÷èùàåòñÿ ñðàçó æå
+	//			//Ã Ã­Ã Ã«Ã®Ã£Ã¨Ã·Ã­Ã®, Ã±Ã²Ã°Ã®ÃªÃ  Ã¬.Ã¡. Ã¯Ã³Ã±Ã²Ã®Ã© Ã¨ Ã­Ã¥ Ã¯Ã³Ã±Ã²Ã®Ã©.
+	//			//Ã¥Ã±Ã«Ã¨ Ã­Ã¥ Ã¯Ã³Ã±Ã²Ã Ã¿, Ã²Ã® Ã²Ã Ã¬ Ã«Ã¨Ã¡Ã® Ã·Ã¨Ã±Ã«Ã®, Ã«Ã¨Ã¡Ã® Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿
+	//			//Ã«Ã¨Ã¡Ã® Ã·Ã²Ã®-Ã²Ã®, Ã·Ã²Ã® Ã¬Ã» Ã¬Ã®Ã¦Ã¥Ã¬ Ã¢Ã»Ã¡Ã°Ã®Ã±Ã¨Ã²Ã¼ Ã¢ Ã®Ã¸Ã¨Ã¡ÃªÃ³)
+	//			//Ã®Ã¯Ã¥Ã°Ã Ã²Ã®Ã°Ã  Ã²Ã Ã¬ Ã¡Ã»Ã²Ã¼ Ã­Ã¥ Ã¬Ã®Ã¦Ã¥Ã², Ã¢ Ã¯Ã°Ã®Ã¸Ã«Ã®Ã¬ if Ã®Ã·Ã¨Ã¹Ã Ã¥Ã²Ã±Ã¿ Ã±Ã°Ã Ã§Ã³ Ã¦Ã¥
 	//			if (!lexema.empty()) {
 	//				if (lexema == sin || lexema == cos || lexema == tg
 	//					|| lexema == ctg) {
 	//					lexems.push_back(lexema);
 	//					lexema = "";
 	//				}
-	//				else if (numbers.count(lexema.back())); //åñëè áûëî ÷èñëî, òî âñå îê, ïðîäîëæàåì
+	//				else if (numbers.count(lexema.back())); //Ã¥Ã±Ã«Ã¨ Ã¡Ã»Ã«Ã® Ã·Ã¨Ã±Ã«Ã®, Ã²Ã® Ã¢Ã±Ã¥ Ã®Ãª, Ã¯Ã°Ã®Ã¤Ã®Ã«Ã¦Ã Ã¥Ã¬
 	//				else throw ("wrong expression, bye-bye");
 	//			}
 	//			lexema += l;
 	//		}
 	//		if (func.count(l)) {
-	//			//åñëè íå ïóñòàÿ ñòðîêà äî,òî ÷òî òàì ìîæåò áûòü?
-	//			//åñëè ÷èñëî, î÷èùàåì
-	//			//åñëè ôóíêöèÿ = òîãäà îíà òî÷íî íåïîëíàÿ, ïîòîìó ÷òî ñåé÷àñ òîæå
-	//			//äîáàâëÿåì áóêâó îò ôóíêöèè
+	//			//Ã¥Ã±Ã«Ã¨ Ã­Ã¥ Ã¯Ã³Ã±Ã²Ã Ã¿ Ã±Ã²Ã°Ã®ÃªÃ  Ã¤Ã®,Ã²Ã® Ã·Ã²Ã® Ã²Ã Ã¬ Ã¬Ã®Ã¦Ã¥Ã² Ã¡Ã»Ã²Ã¼?
+	//			//Ã¥Ã±Ã«Ã¨ Ã·Ã¨Ã±Ã«Ã®, Ã®Ã·Ã¨Ã¹Ã Ã¥Ã¬
+	//			//Ã¥Ã±Ã«Ã¨ Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ = Ã²Ã®Ã£Ã¤Ã  Ã®Ã­Ã  Ã²Ã®Ã·Ã­Ã® Ã­Ã¥Ã¯Ã®Ã«Ã­Ã Ã¿, Ã¯Ã®Ã²Ã®Ã¬Ã³ Ã·Ã²Ã® Ã±Ã¥Ã©Ã·Ã Ã± Ã²Ã®Ã¦Ã¥
+	//			//Ã¤Ã®Ã¡Ã Ã¢Ã«Ã¿Ã¥Ã¬ Ã¡Ã³ÃªÃ¢Ã³ Ã®Ã² Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¨
 	//			if (!lexema.empty()){
-	//				if (numbers.count(lexema.back())){ // êîíåö òî÷íî ÷èñëî
+	//				if (numbers.count(lexema.back())){ // ÃªÃ®Ã­Ã¥Ã¶ Ã²Ã®Ã·Ã­Ã® Ã·Ã¨Ã±Ã«Ã®
 	//					lexems.push_back(lexema);
 	//					lexema = "";
 	//				}
@@ -168,7 +211,7 @@ bool bracketCheck(string str) {
 	//					lexems.push_back(lexema);
 	//					lexema = "";
 	//				}
-	//				//ïîëíîé ëåêñåìû òàì òî÷íî íå ì.á. ò.ê. òîãäà ýòî íåâåðíîå âûðàæåíèå
+	//				//Ã¯Ã®Ã«Ã­Ã®Ã© Ã«Ã¥ÃªÃ±Ã¥Ã¬Ã» Ã²Ã Ã¬ Ã²Ã®Ã·Ã­Ã® Ã­Ã¥ Ã¬.Ã¡. Ã².Ãª. Ã²Ã®Ã£Ã¤Ã  Ã½Ã²Ã® Ã­Ã¥Ã¢Ã¥Ã°Ã­Ã®Ã¥ Ã¢Ã»Ã°Ã Ã¦Ã¥Ã­Ã¨Ã¥
 	//				else throw ("wrong expression, bye-bye");
 	//			}
 	//			lexema += l;
